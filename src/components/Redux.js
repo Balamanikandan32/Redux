cf1d70e27d36f25c750ac6c6c28456622ebe7dd7
Balamanikandan32 <bala34510@gmail.com>
2024-07-30 13:24:11 +0530

    Redux CombineReducers

--- src/components/Redux.js
+++ src/components/Redux.js
@@ -1,4 +1,4 @@
-import {createStore} from "redux"
+import {combineReducers, createStore} from "redux"
 
 const Redux = () => {
   return (
@@ -10,6 +10,7 @@ const Redux = () => {
 export default Redux;
 
 const BUY_CAKE = "buy_cake";
+const BUY_ICECREAM = "buy_icecream"
 
 // Action
 // { type : BUY_CAKE,
@@ -21,13 +22,22 @@ function buycake() {
   return { type: BUY_CAKE, info: "first redux action" };
 }
 
+function buyicecream(){
+    return {
+        type: BUY_ICECREAM,
+    }
+}
+
 // initial state
-const initialState = {
+const initialBuycakeState = {
   numOfCakes: 10,
 };
+const initialIcecreamState = {
+    numOfIcecream : 20,
+}
 
-// reducer function 
-const cakeReducer = (state = initialState, action) => {
+// reducer function for cake
+const cakeReducer = (state = initialBuycakeState, action) => {
   switch (action.type) {
     case "buy_cake":
       return {
@@ -38,13 +48,36 @@ const cakeReducer = (state = initialState, action) => {
       return state;
   }
 };
+ // reduceer fucntion for Icecream
+const icecreamReducer = (state = initialIcecreamState, action)=>{
+    switch(action.type){
+        case "buy_icecream" :
+            return{
+                ...state,
+                numOfIcecream: state.numOfIcecream - 1 ,
+            }
+        default : return state
+    }
+}
+
+
+//combine reducer
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    icecream : icecreamReducer,
+})
 
 // Store 
 
-const store = createStore(cakeReducer)
+const store = createStore(rootReducer)
 console.log("initial state", store.getState())
 const unsubscribe = store.subscribe(()=>console.log("updated state", store.getState()))
 store.dispatch({type : "buy_cake"}) //action
 store.dispatch(buycake()) // action creator
+store.dispatch({type : "buy_icecream"})
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
 unsubscribe()
 store.dispatch({type : "buy_cake"}) // after unsubscribe it stop to state changes will not call subscribe callback fn 
