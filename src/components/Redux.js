65f68e6e8b9fc2974dc77210faa05b56f6d8c65b
Balamanikandan32 <bala34510@gmail.com>
2024-07-30 12:02:49 +0530

    Redux Reducer

--- src/components/Redux.js
+++ src/components/Redux.js
@@ -18,3 +18,21 @@ const BUY_CAKE = "buy_cake";
 function buycake() {
   return { type: BUY_CAKE, info: "first redux action" };
 }
+
+// initial state
+const initialState = {
+  numOfCakes: 10,
+};
+
+// reducer function 
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "buy_cake":
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
